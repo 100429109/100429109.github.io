9fab60b69dbd9a72466c34698228e4c22498d006
AlbaSanz <100429064@alumnos.uc3m.es>
2022-05-03 14:08:42 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AgmCoreModule } from '@agm/core';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { RankingComponent } from './components/ranking/ranking.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {path: 'ranking', component: RankingComponent},
+  {path: 'mapa', component: MapaComponent},
+  {path: 'usuarios', component: UsuariosComponent},
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +31,7 @@ import { RankingComponent } from './components/ranking/ranking.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAyh-MCZk-az-hZlybnOyzkMvZigycO898'
     })
